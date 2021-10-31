5a543f97788e5b110bb06fbb1c38f005a810bc66
BrunaBelo <anabruna28@gmail.com>
2021-10-31 16:33:34 -0300

    Uncomment fields

--- src/components/CreateUser/FormPersonalInformation/index.tsx
+++ src/components/CreateUser/FormPersonalInformation/index.tsx
@@ -18,12 +18,12 @@ const FormPersonalInformation: React.FC = () => {
 
         <InputsCreateUser>
           <Field required type="text" name="name" component={TextField} label="Nome" />
-          {/* <Field required type="email" name="email" component={TextField} label="Email" />
+          <Field required type="email" name="email" component={TextField} label="Email" />
           <Field required type="number" name="phone" component={TextField} label="Telefone" />
           <Field required type="number" name="cpf" component={TextField} label="Cpf" />
           <Field required type="password" name="password" component={TextField} label="Senha" />
           <Field required type="password" name="confirm-password" component={TextField} label="Confirmar Senha" />
-          <Field name="additionalInfs" className="additional-infs" component={TextField} label="Informações Adicionais" multiline rows={3} /> */}
+          <Field name="additionalInfs" className="additional-infs" component={TextField} label="Informações Adicionais" multiline rows={3} />
         </InputsCreateUser>
       </Main>
     </Container>
