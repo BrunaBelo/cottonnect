477b9055a7df2fae6ba82d6c542ac610c868d573
BrunaBelo <anabruna28@gmail.com>
2022-02-02 19:44:47 -0300

    Add link to create account in home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,5 @@
 import react from 'react'
+import { Link } from 'react-router-dom'
 import Navbar from '../../components/Navbar'
 import { Main, Container, TextInfo, HomeImage, Image } from './styles'
 
@@ -11,7 +12,9 @@ export default function Home() {
           <h1>Doe e ganhe!</h1>
           <p>Com a gente você doa e em troca ganha flocos de algodão, que poderam ser usados para
              trocar por outras doações na plataforma!<a> Saiba mais!</a></p>
-          <button>Cadastre-se</button>
+          <Link to="/create-account">
+            <button>Cadastre-se</button>
+          </Link>
         </TextInfo>
         <HomeImage>
           <Image src="/images/main_image_home.png"/>
