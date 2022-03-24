07ed2a7723d5a2d0c4fa8cc32892d4d06ad8fd4d
BrunaBelo <anabruna28@gmail.com>
2022-03-24 20:30:18 -0300

    Add protected routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,26 +1,39 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import CreateUser from "./components/CreateUser";
-import Login from "./components/Login";
+import CreateUser from "./pages/CreateUser";
+import Login from "./pages/login";
 import DonationDetails from "./pages/donation-details";
 import Explorer from "./pages/explorer";
 import Home from "./pages/home";
 import Logout from "./pages/logout";
 import NewDonation from "./pages/new-donation";
+import ProtectedRoute from "./components/protected-route";
+import GuestRoute from "./components/guest-route"
 
 export default function AppRoutes(){
+
+  const setAsProtected = (Component: JSX.Element) => {
+    return <ProtectedRoute component={Component} />
+  }
+
+  const setAsGuest = (Component: JSX.Element) => {
+    return <GuestRoute component={Component} />
+  }
+
   return(
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={setAsGuest(<Home/>)}/>
+        <Route path="/login" element={setAsGuest(<Login/>)}/>
+        <Route path="/create-account" element={setAsGuest(<CreateUser/>)}/>
         <Route path="/about"/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/logout" element={<Logout/>}/>
-        <Route path="/create-account" element={<CreateUser/>}/>
-        <Route path="/explorer" element={<Explorer/>}/>
-        <Route path="/new-donation" element={<NewDonation/>}/>
-        <Route path="/donation/:id" element={<DonationDetails/>}/>
+        <Route path="/app">
+          <Route path="logout" element={setAsProtected(<Logout/>)}/>
+          <Route path="explorer" element={setAsProtected(<Explorer/>)}/>
+          <Route path="new-donation" element={setAsProtected(<NewDonation/>)}/>
+          <Route path="donation/:id" element={setAsProtected(<DonationDetails/>)}/>
+        </Route>
       </Routes>
     </BrowserRouter>
   )
