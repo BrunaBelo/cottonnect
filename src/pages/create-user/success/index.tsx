b81bd50d0897ccf7416bdd9b14b0e19d5baef0d2
BrunaBelo <anabruna28@gmail.com>
2022-05-16 10:08:44 -0300

    Add information to user confirm your account

--- src/pages/create-user/success/index.tsx
+++ src/pages/create-user/success/index.tsx
@@ -10,6 +10,7 @@ function renderMain(){
   return(
     <Main>
       <SubTitle>Sua conta foi registrada, agora é só você fazer login e já pode começar doar!</SubTitle>
+      <h3>Acesse seu email e confirme sua conta através de um email que foi enviado para você!</h3>
       <ImageDone>
         <DoneIcon src="/images/done.png" alt="Conta Criada" />
       </ImageDone>
